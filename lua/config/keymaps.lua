84a692ff5600047b1956738e254311a49c261949
Sharif Elkassed <selkasse@gmu.edu>
2025-01-23 16:26:07 -0500

    commit before terminal config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,3 @@
--- REMAPS
---[[ these are no longer needed with the nightly verson of neovim
-vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
-vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
-vim.keymap.set('n', 'grr', vim.lsp.buf.references)
---]]
--- - "grn" is mapped in Normal mode to |vim.lsp.buf.rename()|
--- - "gra" is mapped in Normal and Visual mode to |vim.lsp.buf.code_action()|
--- - "grr" is mapped in Normal mode to |vim.lsp.buf.references()|
--- - "gri" is mapped in Normal mode to |vim.lsp.buf.implementation()|
--- - "gO" is mapped in Normal mode to |vim.lsp.buf.document_symbol()|
--- -  CTRL-S is mapped in Insert mode to |vim.lsp.buf.signature_help()|
-
 -- run whole file
 vim.keymap.set('n', '<space><space>x', '<cmd>source %<CR>')
 
@@ -40,3 +27,6 @@ vim.keymap.set('n', '<space>qc', '<cmd>call setqflist([], "f")<CR>')
 -- remap Option + c for copy
 vim.api.nvim_set_keymap('n', '<A-c>', '"+y', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<A-c>', '"+y', { noremap = true, silent = true })
+
+-- terminal
+--vim.keymap.set
