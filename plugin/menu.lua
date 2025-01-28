61586088bced4cf2b96cdd618554b978aff697c2
Sharif Elkassed <selkasse@gmu.edu>
2025-01-28 02:29:16 -0500

    add comments to menu.lua

--- plugin/menu.lua
+++ plugin/menu.lua
@@ -6,6 +6,7 @@ anoremenu PopUp.-1-                     <Nop>
 anoremenu PopUp.Inspect                 <Cmd>Inspect<CR>
 amenu     PopUp.Open\ in\ web\ browser  gx
 anoremenu PopUp.References              <cmd>Telescope lsp_references<CR>
+"anoremenu PopUp.Open\ Inventory               <cmd>echo "open inventory" <CR>
 
 ]]
 
@@ -17,18 +18,22 @@ vim.api.nvim_create_autocmd('MenuPopup', {
   group = group,
   desc = "Custom PopUp Setup",
   callback = function()
-    -- Only show Go to Definition if there is an LSP attached
+    -- Only show certain menu items if there is an LSP attached
     vim.cmd [[
       amenu disable PopUp.Go\ to\ definition
       amenu disable PopUp.References
+      "amenu disable PopUp.Inspect
     ]]
 
+    -- Comment this section to remove the items from the menu
     if vim.lsp.get_clients({ bufnr = 0 })[1] then
       vim.cmd [[
         amenu enable PopUp.Go\ to\ definition
         amenu enable PopUp.References
       ]]
     end
+    -- Comment this section to remove the items from the menu
+
 
     local urls = require('vim.ui')._get_urls()
     if vim.startswith(urls[1], 'http') then
